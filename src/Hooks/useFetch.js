3aeb6cd9761c1d029f211a2ffe077995096964d2
David Silva <davidlsilva.sp@gmail.com>
2021-01-18 12:45:21 -0300

    Tentativa de correção de cors

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,5 +1,5 @@
 const useFetch = (endpoint) => {
-  var url = ""
+  var url = "";
   if (endpoint === "tournaments") {
     url = "https://api.pandascore.co/leagues/302";
   } else {
@@ -8,12 +8,14 @@ const useFetch = (endpoint) => {
 
   const config = {
     method: "get",
+    mode: "cors",
     url: url + "/" + endpoint,
     headers: {
       Authorization:
         "Bearer kqx262D7qNXlM7I68k8ssl7FSqiIO81SHUOpQhHnXkoWTahiHzs",
+      "Content-Type": "application/json",
     },
-  }
+  };
 
   return config;
 };
