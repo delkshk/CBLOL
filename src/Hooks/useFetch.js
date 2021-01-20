7cb80d8231b7bbe8d314262d364b1dc00f75cf83
David Silva <davidlsilva.sp@gmail.com>
2021-01-20 10:03:15 -0300

    Ajustes na api

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,20 +1,14 @@
 const useFetch = (endpoint) => {
-  var url = "";
+  var url = "https://cblolapi.herokuapp.com/";
   if (endpoint === "tournaments") {
     url = "https://api.pandascore.co/leagues/302";
   } else {
-    url = "https://api.pandascore.co/tournaments/5329";
+    url = url + "tabela";
   }
-
   const config = {
-    method: "get",
-    mode: "cors",
-    url: url + "/" + endpoint,
-    headers: {
-      Authorization:
-        "Bearer kqx262D7qNXlM7I68k8ssl7FSqiIO81SHUOpQhHnXkoWTahiHzs",
-      "Content-Type": "application/json",
-    },
+    method: 'get',
+    url: url,
+    headers: {"Content-Type": "application/json"}
   };
 
   return config;
